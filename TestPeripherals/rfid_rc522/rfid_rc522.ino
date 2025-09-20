f010c0d24caeb3c12eb5ffb7e3845b3b412c2692
thaidao <daonamthai@gmail.com>
2025-09-20 17:05:49 -0400

    add:test rfid rc522 with esp32 nano

---
+++ TestPeripherals/rfid_rc522/rfid_rc522.ino
@@ -0,0 +1,45 @@
+/*
+ * This Arduino Nano ESP32 code was developed by newbiely.com
+ *
+ * This Arduino Nano ESP32 code is made available for public use without any restriction
+ *
+ * For comprehensive instructions and wiring diagrams, please visit:
+ * https://newbiely.com/tutorials/arduino-nano-esp32/arduino-nano-esp32-rfid
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN  D10 // Arduino Nano ESP32 pin connected to RC522's SS pin
+#define RST_PIN D5  // Arduino Nano ESP32 pin connected to RC522's RST pin
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin(); // init SPI bus
+  rfid.PCD_Init(); // init MFRC522
+
+  Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader");
+}
+
+void loop() {
+  if (rfid.PICC_IsNewCardPresent()) { // new tag is available
+    if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      Serial.print("RFID/NFC Tag Type: ");
+      Serial.println(rfid.PICC_GetTypeName(piccType));
+
+      // print UID in Serial Monitor in the hex format
+      Serial.print("UID:");
+      for (int i = 0; i < rfid.uid.size; i++) {
+        Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(rfid.uid.uidByte[i], HEX);
+      }
+      Serial.println();
+
+      rfid.PICC_HaltA(); // halt PICC
+      rfid.PCD_StopCrypto1(); // stop encryption on PCD
+    }
+  }
+}
